9c5d43deee887ba2fe14111c942a58f442a5407b
Maurelian <maurelian@protonmail.ch>
2024-12-05 23:44:26 -0500

    very noisy but working Upgrade.s.sol

--- packages/contracts-bedrock/test/setup/Upgrade.s.sol
+++ packages/contracts-bedrock/test/setup/Upgrade.s.sol
@@ -18,23 +18,16 @@ import { Process } from "scripts/libraries/Process.sol";
 contract Upgrade is Deploy {
     using stdJson for string;
 
-    string internal _json;
+    string internal _stateJson;
+    string internal _stdVersionsToml;
 
     /// @notice Deploy all of the L1 contracts necessary for a full Superchain with a single Op Chain.
 
     function run() public override {
-        console.log("Deploying fresh Superchain Shared contracts");
-
-        // ----- Deploy the Superchain Shared contracts -----
-        // TODO: replace this with a call to `op-deployer bootstrap superchain`
-        // Note: for the moment deploySuperchain() is unnecessary, because testing is using a fork of mainnet,
-        //       so
-        deploySuperchain();
+        // Note: For now we are using a fork of mainnet, so we don't need to deploy the Superchain Shared contracts.
+        //       This will change after the next release when we have all implementations in a single release.
 
         // ----- Deploy the OPCM along with implementations contracts it requires -----
-        // WIP: this currently only works when anvil is run by forking mainnet, otherwise it fails with:
-        // "error getting superchain config: unsupported chain ID: 31337\n".
-        // One option for fixing would be to add 31337 to a standard-versions toml file
         string[] memory cmds = new string[](9);
         cmds[0] = "scripts/op-deployer";
         cmds[1] = "bootstrap";
@@ -50,7 +43,7 @@ contract Upgrade is Deploy {
         bytes memory result = Process.run(cmds);
         console.log(string(result));
 
-        string[] memory cmds2 = new string[](7);
+        string[] memory cmds2 = new string[](8);
         cmds2[0] = "scripts/op-deployer";
         cmds2[1] = "apply";
         cmds2[2] = "--l1-rpc-url";
@@ -58,53 +51,134 @@ contract Upgrade is Deploy {
         cmds2[4] = "--private-key";
         // Private key for first default hardhat account
         cmds2[5] = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+        cmds2[6] = "--workdir";
+        cmds2[7] = "test/fixtures";
 
         bytes memory result2 = Process.run(cmds2);
         console.log(string(result2));
-        // TODO: Save OPCM addresses from state.json
 
-        console.log("DeployConfig: reading file %s", "state.json");
-        try vm.readFile("state.json") returns (string memory data_) {
-            _json = data_;
+        console.log("Upgrade: reading file state.json");
+        try vm.readFile("test/fixtures/state.json") returns (string memory data_) {
+            _stateJson = data_;
         } catch {
-            require(false, string.concat("DeployConfig: cannot find deploy config file at ", "state.json"));
+            require(false, "Upgrade: cannot find state.json");
         }
 
-        save("SuperchainConfig", _json.readAddress("$.opChainDeployments[0].superchainConfigImplAddress"));
-        save("SuperchainConfigProxy", _json.readAddress("$.opChainDeployments[0].superchainConfigProxyAddress"));
-        save("ProtocolVersions", _json.readAddress("$.opChainDeployments[0].protocolVersions"));
-        save("ProtocolVersionsProxy", _json.readAddress("$.opChainDeployments[0].protocolVersionsProxy"));
-        save("SuperchainProxyAdmin", _json.readAddress("$.opChainDeployments[0].superchainProxyAdmin"));
-        save("SuperchainConfigProxy", _json.readAddress("$.opChainDeployments[0].superchainConfigProxy"));
-        save("SuperchainConfig", _json.readAddress("$.opChainDeployments[0].superchainConfig"));
-        save("ProtocolVersionsProxy", _json.readAddress("$.opChainDeployments[0].protocolVersionsProxy"));
-        save("ProtocolVersions", _json.readAddress("$.opChainDeployments[0].protocolVersions"));
-        save("L1CrossDomainMessenger", _json.readAddress("$.opChainDeployments[0].l1CrossDomainMessenger"));
-        save("OptimismMintableERC20Factory", _json.readAddress("$.opChainDeployments[0].optimismMintableERC20Factory"));
-        save("SystemConfig", _json.readAddress("$.opChainDeployments[0].systemConfig"));
-        save("L1StandardBridge", _json.readAddress("$.opChainDeployments[0].l1StandardBridge"));
-        save("L1ERC721Bridge", _json.readAddress("$.opChainDeployments[0].l1ERC721Bridge"));
-        save("OptimismPortal2", _json.readAddress("$.opChainDeployments[0].optimismPortal2"));
-        save("DisputeGameFactory", _json.readAddress("$.opChainDeployments[0].disputeGameFactory"));
-        save("DelayedWETH", _json.readAddress("$.opChainDeployments[0].delayedWETH"));
-        save("PreimageOracle", _json.readAddress("$.opChainDeployments[0].preimageOracle"));
-        save("Mips", _json.readAddress("$.opChainDeployments[0].mips"));
-        save("OPContractsManager", _json.readAddress("$.opChainDeployments[0].oPContractsManager"));
-        save("ProxyAdmin", _json.readAddress("$.opChainDeployments[0].proxyAdmin"));
-        save("AddressManager", _json.readAddress("$.opChainDeployments[0].addressManager"));
-        save("L1ERC721BridgeProxy", _json.readAddress("$.opChainDeployments[0].l1ERC721BridgeProxy"));
-        save("SystemConfigProxy", _json.readAddress("$.opChainDeployments[0].systemConfigProxy"));
+        // Save superchain shared contracts
+        save("SuperchainConfig", _stateJson.readAddress("$.superchainDeployment.superchainConfigImplAddress"));
+        save("SuperchainConfigProxy", _stateJson.readAddress("$.superchainDeployment.superchainConfigProxyAddress"));
+        save("ProtocolVersions", _stateJson.readAddress("$.superchainDeployment.protocolVersionsImplAddress"));
+        save("ProtocolVersionsProxy", _stateJson.readAddress("$.superchainDeployment.protocolVersionsProxyAddress"));
+        save("SuperchainProxyAdmin", _stateJson.readAddress("$.superchainDeployment.proxyAdminAddress"));
+
+        console.log("Upgrade: reading file %s", "test/fixtures/standard-versions.toml");
+        try vm.readFile("test/fixtures/standard-versions.toml") returns (string memory data_) {
+            _stdVersionsToml = data_;
+        } catch {
+            require(false, "Upgrade: cannot find standard-versions.toml");
+        }
+
+        vm.parseToml(_stdVersionsToml, ".releases[\"op-contracts/v1.6.0\"]");
+        // Save OPCM contracts
+        save(
+            "L1CrossDomainMessenger",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(".releases[\"op-contracts/v1.6.0\"].l1_cross_domain_messenger.implementation_address")
+            )
+        );
+        save(
+            "OptimismMintableERC20Factory",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(
+                    ".releases[\"op-contracts/v1.6.0\"].optimism_mintable_erc20_factory.implementation_address"
+                )
+            )
+        );
+        save(
+            "SystemConfig",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(".releases[\"op-contracts/v1.6.0\"].system_config.implementation_address")
+            )
+        );
+        save(
+            "L1StandardBridge",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(".releases[\"op-contracts/v1.6.0\"].l1_standard_bridge.implementation_address")
+            )
+        );
+        save(
+            "L1ERC721Bridge",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(".releases[\"op-contracts/v1.6.0\"].l1_erc721_bridge.implementation_address")
+            )
+        );
+        save(
+            "OptimismPortal2",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(".releases[\"op-contracts/v1.6.0\"].optimism_portal.implementation_address")
+            )
+        );
+        save(
+            "DisputeGameFactory",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(".releases[\"op-contracts/v1.6.0\"].dispute_game_factory.implementation_address")
+            )
+        );
+        save(
+            "DelayedWETHProxy",
+            vm.parseTomlAddress(
+                _stdVersionsToml,
+                string.concat(".releases[\"op-contracts/v1.6.0\"].delayed_weth.implementation_address")
+            )
+        );
+        save(
+            "PreimageOracle",
+            vm.parseTomlAddress(
+                _stdVersionsToml, string.concat(".releases[\"op-contracts/v1.6.0\"].preimage_oracle.address")
+            )
+        );
+        save(
+            "Mips",
+            vm.parseTomlAddress(_stdVersionsToml, string.concat(".releases[\"op-contracts/v1.6.0\"].mips.address"))
+        );
+
+        // save("OPContractsManager", _json.readAddress("$.opChainDeployments[0].opContractsManager"));
+
+        save("ProxyAdmin", _stateJson.readAddress("$.opChainDeployments[0].proxyAdminAddress"));
+        save("AddressManager", _stateJson.readAddress("$.opChainDeployments[0].addressManagerAddress"));
+        save("L1ERC721BridgeProxy", _stateJson.readAddress("$.opChainDeployments[0].l1ERC721BridgeProxyAddress"));
+        save("SystemConfigProxy", _stateJson.readAddress("$.opChainDeployments[0].systemConfigProxyAddress"));
         save(
             "OptimismMintableERC20FactoryProxy",
-            _json.readAddress("$.opChainDeployments[0].optimismMintableERC20FactoryProxy")
+            _stateJson.readAddress("$.opChainDeployments[0].optimismMintableERC20FactoryProxyAddress")
         );
-        save("L1StandardBridgeProxy", _json.readAddress("$.opChainDeployments[0].l1StandardBridgeProxy"));
-        save("L1CrossDomainMessengerProxy", _json.readAddress("$.opChainDeployments[0].l1CrossDomainMessengerProxy"));
-        save("DisputeGameFactoryProxy", _json.readAddress("$.opChainDeployments[0].disputeGameFactoryProxy"));
-        save("PermissionedDelayedWETHProxy", _json.readAddress("$.opChainDeployments[0].permissionedDelayedWETHProxy"));
-        save("AnchorStateRegistryProxy", _json.readAddress("$.opChainDeployments[0].anchorStateRegistryProxy"));
-        save("AnchorStateRegistry", _json.readAddress("$.opChainDeployments[0].anchorStateRegistry"));
-        save("PermissionedDisputeGame", _json.readAddress("$.opChainDeployments[0].permissionedDisputeGame"));
-        save("OptimismPortalProxy", _json.readAddress("$.opChainDeployments[0].optimismPortalProxy"));
+        save("L1StandardBridgeProxy", _stateJson.readAddress("$.opChainDeployments[0].l1StandardBridgeProxyAddress"));
+        save(
+            "L1CrossDomainMessengerProxy",
+            _stateJson.readAddress("$.opChainDeployments[0].l1CrossDomainMessengerProxyAddress")
+        );
+        save(
+            "DisputeGameFactoryProxy", _stateJson.readAddress("$.opChainDeployments[0].disputeGameFactoryProxyAddress")
+        );
+        save(
+            "PermissionedDelayedWETHProxy",
+            _stateJson.readAddress("$.opChainDeployments[0].delayedWETHPermissionedGameProxyAddress")
+        );
+        save(
+            "AnchorStateRegistryProxy",
+            _stateJson.readAddress("$.opChainDeployments[0].anchorStateRegistryProxyAddress")
+        );
+        save("AnchorStateRegistry", _stateJson.readAddress("$.opChainDeployments[0].anchorStateRegistryImplAddress"));
+        save(
+            "PermissionedDisputeGame", _stateJson.readAddress("$.opChainDeployments[0].permissionedDisputeGameAddress")
+        );
+        save("OptimismPortalProxy", _stateJson.readAddress("$.opChainDeployments[0].optimismPortalProxyAddress"));
     }
 }
