793327df7fc15f4e47a6f99ff11975cd1c9d26d6
Adrian Sutton <adrian@oplabs.co>
2024-12-10 12:02:56 +1000

    deploy-script: Add post-deployment checks

--- packages/contracts-bedrock/scripts/deploy/DeployPermissionless.sol
+++ packages/contracts-bedrock/scripts/deploy/DeployPermissionless.sol
@@ -19,6 +19,7 @@ import {GameType, Duration, Claim} from "../../src/dispute/lib/LibUDT.sol";
 import {GameTypes} from "../../src/dispute/lib/Types.sol";
 import {DisputeGameFactory} from "../../src/dispute/DisputeGameFactory.sol";
 import {Constants} from "../../src/libraries/Constants.sol";
+import {ChainAssertions} from "./ChainAssertions.sol";
 
 contract DeployPermissionless is Script {
 
@@ -34,10 +35,14 @@ contract DeployPermissionless is Script {
         DelayedWETH permDelayedWeth = DelayedWETH(payable(address(permissioned.weth())));
         initializeDelayedWethProxy(permDelayedWeth, proxy);
         transferDelayedWethProxyAdmin(permDelayedWeth, proxy);
-        console.log("DelayedWethProxy:", address(proxy));
 
-        deployFaultDisputeGame(permissioned, IDelayedWETH(address(proxy)));
-        deployPermissionedDisputeGame(permissioned);
+        FaultDisputeGame fdg = deployFaultDisputeGame(permissioned, IDelayedWETH(address(proxy)));
+        PermissionedDisputeGame pdg = deployPermissionedDisputeGame(permissioned);
+
+        checkDelayedWeth(permDelayedWeth, DelayedWETH(payable(address(proxy))));
+        checkFaultDisputeGame(permissioned, fdg, IDelayedWETH(address(proxy)));
+        checkPermissionedDisputeGame(permissioned, pdg);
+        printDeploymentSummary(address(proxy), address(fdg), address(pdg));
     }
 
     function deployDelayedWethProxy() internal broadcast returns (Proxy) {
@@ -58,7 +63,7 @@ contract DeployPermissionless is Script {
         _proxy.changeAdmin(proxyAdmin);
     }
 
-    function deployFaultDisputeGame(PermissionedDisputeGame _permissioned, IDelayedWETH _delayedWeth) internal broadcast {
+    function deployFaultDisputeGame(PermissionedDisputeGame _permissioned, IDelayedWETH _delayedWeth) internal broadcast returns (FaultDisputeGame) {
         console.log("Deploying FaultDisputeGame");
 
         FaultDisputeGame impl = new FaultDisputeGame(
@@ -73,10 +78,10 @@ contract DeployPermissionless is Script {
             _permissioned.anchorStateRegistry(),
             _permissioned.l2ChainId()
         );
-        console.log("FaultDisputeGame: ", address(impl));
+        return impl;
     }
 
-    function deployPermissionedDisputeGame(PermissionedDisputeGame _permissioned) internal broadcast {
+    function deployPermissionedDisputeGame(PermissionedDisputeGame _permissioned) internal broadcast returns (PermissionedDisputeGame) {
         console.log("Deploying PermissionedDisputeGame");
         PermissionedDisputeGame impl = new PermissionedDisputeGame(
             GameTypes.PERMISSIONED_CANNON,
@@ -92,7 +97,47 @@ contract DeployPermissionless is Script {
             _permissioned.proposer(),
             _permissioned.challenger()
         );
-        console.log("PermissionedDisputeGame: ", address(impl));
+        return impl;
+    }
+
+    function checkDelayedWeth(DelayedWETH _expected, DelayedWETH _impl) internal view {
+        require(_impl.owner() == _expected.owner(), "WETH-10");
+        require(_impl.delay() == _expected.delay(), "WETH-20");
+        require(_impl.config() == _expected.config(), "WETH-30");
+    }
+
+    function checkFaultDisputeGame(PermissionedDisputeGame _permissioned, FaultDisputeGame _impl, IDelayedWETH _delayedWeth) internal view {
+        require(_impl.gameType().raw() == GameTypes.CANNON.raw(), "FDG-10");
+        require(_impl.absolutePrestate().raw() == _absolutePrestate, "FDG-20");
+        require(_impl.maxGameDepth() == _permissioned.maxGameDepth(), "FDG-30");
+        require(_impl.splitDepth() == _permissioned.splitDepth(), "FDG-40");
+        require(_impl.clockExtension().raw() == _permissioned.clockExtension().raw(), "FDG-50");
+        require(_impl.maxClockDuration().raw() == _permissioned.maxClockDuration().raw(), "FDG-60");
+        require(_impl.vm() == _permissioned.vm(), "FDG-70");
+        require(_impl.weth() == _delayedWeth, "FDG-80");
+        require(_impl.anchorStateRegistry() == _permissioned.anchorStateRegistry(), "FDG-90");
+        require(_impl.l2ChainId() == _permissioned.l2ChainId(), "FDG-100");
+    }
+
+    function checkPermissionedDisputeGame(PermissionedDisputeGame _permissioned, PermissionedDisputeGame _impl) internal view {
+        require(_impl.gameType().raw() == GameTypes.PERMISSIONED_CANNON.raw(), "PDG-10");
+        require(_impl.absolutePrestate().raw() == _absolutePrestate, "PDG-20");
+        require(_impl.maxGameDepth() == _permissioned.maxGameDepth(), "PDG-30");
+        require(_impl.splitDepth() == _permissioned.splitDepth(), "PDG-40");
+        require(_impl.clockExtension().raw() == _permissioned.clockExtension().raw(), "PDG-50");
+        require(_impl.maxClockDuration().raw() == _permissioned.maxClockDuration().raw(), "PDG-60");
+        require(_impl.vm() == _permissioned.vm(), "PDG-70");
+        require(_impl.weth() == _permissioned.weth(), "PDG-80");
+        require(_impl.anchorStateRegistry() == _permissioned.anchorStateRegistry(), "PDG-90");
+        require(_impl.l2ChainId() == _permissioned.l2ChainId(), "PDG-100");
+    }
+
+    /// @notice Prints a summary of the contracts deployed during this script.
+    function printDeploymentSummary(address _wethProxy, address _fdg, address _pdg) internal view {
+        console.log("Deployment Summary (chainid: %d)", block.chainid);
+        console.log("    0. DelayedWETHProxy: %s", _wethProxy);
+        console.log("    1. FaultDisputegame: %s", _fdg);
+        console.log("    2. PermissionedDisputeGame: %s", _pdg);
     }
 
     ////////////////////////////////////////////////////////////////
