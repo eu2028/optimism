1b2b6e05cf5faa304886bb105144e8e4183c296e
Yann Hodique <yann@oplabs.co>
2024-12-19 15:19:14 +0100

    feat: add deployer cmd

---
+++ kurtosis-devnet/pkg/kurtosis/sources/deployer/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ethereum-optimism/optimism/kurtosis-devnet/pkg/kurtosis/sources/deployer"
+)
+
+func main() {
+	// Parse command line flags
+	enclave := flag.String("enclave", "", "Name of the Kurtosis enclave")
+	flag.Parse()
+
+	if *enclave == "" {
+		fmt.Fprintln(os.Stderr, "Error: --enclave flag is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Get deployer data
+	data, err := deployer.ParseDeployerData(*enclave)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing deployer data: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Encode as JSON and write to stdout
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(data); err != nil {
+		fmt.Fprintf(os.Stderr, "Error encoding JSON: %v\n", err)
+		os.Exit(1)
+	}
+}
